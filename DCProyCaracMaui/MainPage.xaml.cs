60b464deff9d431c2c93410b2b717268b320846d
Cheboy04 <cornejoadrian93@gmail.com>
2024-12-16 10:31:49 -0500

    Completo

--- DCProyCaracMaui/MainPage.xaml.cs
+++ DCProyCaracMaui/MainPage.xaml.cs
@@ -2,23 +2,46 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        public void GetDeviceInfo(object sender, EventArgs e)
         {
-            count++;
+            InfoDevice.Text = $"Fabricante: {DeviceInfo.Manufacturer}\n" +
+                              $"Modelo: {DeviceInfo.Model}\n" +
+                              $"Nombre: {DeviceInfo.Name}\n" +
+                              $"Plataforma: {DeviceInfo.Platform}\n" +
+                              $"OS Version: {DeviceInfo.VersionString}\n" + 
+                              $"Idioma: {DeviceInfo.Idiom}";
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        public void OnVibrateClicked(object sender, EventArgs e)
+        {
+            Vibration.Vibrate();
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        public void GetBatteryClicked(object sender, EventArgs e)
+        {
+            var level = Battery.ChargeLevel * 100;
+            var state = Battery.State;
+            var source = Battery.PowerSource;
+
+            InfoBattery.Text = $"Nivel de bateria: {level}%\n " +
+                               $"Estado: {state}\n" +
+                               $"Fuente: {source}";
+        }
+
+        public async void TakePhotoClicked(object sender, EventArgs e)
+        {
+            var photo = await MediaPicker.CapturePhotoAsync();
+            if (photo != null)
+            {
+                PhotoImage.Source = ImageSource.FromStream(() => photo.OpenReadAsync().Result);
+            }
+
         }
     }
 
