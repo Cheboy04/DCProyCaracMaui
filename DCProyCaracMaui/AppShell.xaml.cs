c382bdb27d5edb23b5cb6de9caebf7d31f5e9f6b
cisco <cisco@UPEAULA-31713>
2024-12-16 09:27:21 -0500

    Agregar archivos de proyecto.

---
+++ DCProyCaracMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DCProyCaracMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DCProyCaracMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DCProyCaracMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
